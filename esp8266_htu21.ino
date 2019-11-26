beda884628baaa871988ccdb63e9e62487b19f36
Dmitry Abashin <abashin.d@gmail.com>
2019-11-26 21:57:33 +0500

    Manual mode for mobile device was added

--- esp8266_htu21.ino
+++ esp8266_htu21.ino
@@ -3,46 +3,71 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-#define CONTROL_PIN D7
+#define CONTROL_PIN 13
 
-char auth[] = "***********************";
+char auth[] = "***";
 char ssid[] = "7SkyHome";
-char pass[] = "**********************";
- 
+char pass[] = "***";
+
+float hum = 0;
+float temp = 0;
+float dew_point = 0;
+int manual_mode = 1;
+int humidifier_state = 0;
+
+BlynkTimer timer;
+
+BLYNK_WRITE(V3)
+{
+  humidifier_state = param.asInt();
+}
+
+BLYNK_WRITE(V4)
+{
+  manual_mode = param.asInt();
+}
+
 void setup()
 {
 Serial.begin(9600);
 Blynk.begin(auth, ssid, pass);
 Wire.begin();
 pinMode(CONTROL_PIN, OUTPUT);
+timer.setInterval(1000L, do_it);
 }
- 
-void loop()
-{
-float hum = SHT2x.GetHumidity();
-float temp = SHT2x.GetTemperature();
-float dew_point = SHT2x.GetDewPoint();
-int humidifierState;
 
-Serial.println("Humidity(%RH): " + String(hum));
+void do_it()
+{
+hum = SHT2x.GetHumidity();
+temp = SHT2x.GetTemperature();
+dew_point = SHT2x.GetDewPoint();
+
+Serial.println(" Humidity(%RH): " + String(hum));
 Serial.println(" Temperature(C): " + String(temp));
 Serial.println(" Dewpoint(C): " + String(dew_point));
+Serial.println(" Manual mode: " + String(manual_mode));
+Serial.println(" Humidifier state: " + String(humidifier_state));
 
-if(hum <= 30)
-    humidifierState = 1;
-else
-    humidifierState = 0;
-
-if(humidifierState == 1)
+if(humidifier_state == 1)
   digitalWrite(CONTROL_PIN, HIGH);
 else
   digitalWrite(CONTROL_PIN, LOW);
 
-Blynk.run();
+if (manual_mode != 1)
+{
+  if(hum <= 30)
+    humidifier_state = 1;
+  else
+    humidifier_state = 0;
+  Blynk.virtualWrite(V3, humidifier_state);
+}
 Blynk.virtualWrite(V0, hum);
 Blynk.virtualWrite(V1, temp);
 Blynk.virtualWrite(V2, dew_point);
-Blynk.virtualWrite(V3, dew_point);
-
-delay(1000);
+}
+ 
+void loop()
+{
+Blynk.run();
+timer.run();
 }
